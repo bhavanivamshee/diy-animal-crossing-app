8ec4786b07c1b2b190f7eff79c84370bf37df8e7
Bhavani <bhavanivamshee@gmail.com>
2021-05-26 15:14:15 -0500

    adds log out button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,19 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to users_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:island_name, :username, :email, :dream_address, :password, :password_confirmation)
     end
 
 

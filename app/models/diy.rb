27f50440537e7e288251c7f0aa4cbe26932124d3
Bhavani <bhavanivamshee@gmail.com>
2021-06-01 23:03:29 -0500

    adds validations to DIY

--- app/models/diy.rb
+++ app/models/diy.rb
@@ -2,4 +2,6 @@ class Diy < ApplicationRecord
     serialize :materials_diy, HashSerializer
     has_many :materials
     has_many :users, through: :materials
+
+    validates :name, presence: true, uniqueness: true
 end
